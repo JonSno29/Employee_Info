1cee3bd405ae416d847f49ecc6e9a479f0c86c23
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-11-07 18:45:50 -0700

    created intern card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -31,3 +31,19 @@ const engineersCard = function(engineer) {
         </div>
     </div>`
 };
+const internsCard = function(intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">school</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>`
+};
